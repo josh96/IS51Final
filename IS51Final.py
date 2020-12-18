cf09797c97a3c59a07c9374b8c04b563637ec55e
Josh Paredez <joshparedez96@Joshs-MacBook-Air.local>
2020-12-17 18:56:15 -0800

    finish pseudocode

--- IS51Final.py
+++ IS51Final.py
@@ -2,4 +2,14 @@
 There will be a list of student grades from the final exam. The program will display
 the number of grades. It will also display the average grade as well as displaying 
 the percentage of grades that are abover the average.
+
 """
+def(grades): (#list of grades)
+  #avg grade == 83.25#
+  #number of grades = 24#
+  #percent of grades above avg = 54.17#
+
+
+#count how many grades are above the average of 83.25
+(number of grades above avg) / 24 = ans(percent of grades above avg)
+#answer is the percent of grades above the average grade
