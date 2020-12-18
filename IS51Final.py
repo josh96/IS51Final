f7381de8c1b43081557c3a88244b9b379b8397d1
Josh Paredez <joshparedez96@Joshs-MacBook-Air.local>
2020-12-17 18:50:32 -0800

    finish structured-english

--- IS51Final.py
+++ IS51Final.py
@@ -1,5 +1,12 @@
 """
 There will be a list of student grades from the final exam. The program will display
 the number of grades. It will also display the average grade as well as displaying 
-the percentage of grades that are abover the average.
+the percentage of grades that are abover the average. The program will count how many
+grades are above the average grade of 83.25. It will take this number and divide by the 
+total amount of tests graded. This will equal the percentage of grades above the average.
+
+main
 """
+
+
+
