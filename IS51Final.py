1290ca90faf195f4147fa7192121d4bde4160875
Josh Paredez <joshparedez96@Joshs-MacBook-Air.local>
2020-12-17 18:27:19 -0800

    finish structured-english

--- IS51Final.py
+++ IS51Final.py
@@ -1,3 +1,5 @@
 """
-
+There will be a list of student grades from the final exam. The program will display
+the number of grades. It will also display the average grade as well as displaying 
+the percentage of grades that are abover the average.
 """
