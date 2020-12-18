db6b65f6bb18806046a11ac5fa97aa3a4b5062cc
Josh Paredez <joshparedez96@Joshs-MacBook-Air.local>
2020-12-17 19:03:45 -0800

    finish exam-end

--- IS51Final.py
+++ IS51Final.py
@@ -13,3 +13,12 @@ def(grades): (#list of grades)
 #count how many grades are above the average of 83.25
 (number of grades above avg) / 24 = ans(percent of grades above avg)
 #answer is the percent of grades above the average grade
+
+def main()
+  question = "What is the average grade"
+  answer = "The average grade is 83.25"
+  total grades = "There are 24 grades in total"
+  list of grades = (#list of grades)
+
+  sum(list of grades) / total grades == answer:
+    print(answer)
