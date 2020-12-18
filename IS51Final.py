5a9330b72b39223adf3d315953f000e0e573bd38
Josh Paredez <joshparedez96@Joshs-MacBook-Air.local>
2020-12-17 19:19:56 -0800

    finish structured-english
    '
    clear
    pwd

--- IS51Final.py
+++ IS51Final.py
@@ -4,6 +4,8 @@ the number of grades. It will also display the average grade as well as displayi
 the percentage of grades that are abover the average. The program will count how many
 grades are above the average grade of 83.25. It will take this number and divide by the 
 total amount of tests graded. This will equal the percentage of grades above the average.
+To find the percentage of grades above average we must divide the amount of scores above
+the average score by the amount of scores below the average.
 
 main
 """
